604fcbd7a4a2ce2074bfce9266ab1f57d0a3f2f0
bugakov <bugakovru@gmail.com>
2013-12-03 16:01:50 +0300

    Russian language files added

---
+++ core/components/ajaxupload/lexicon/ru/properties.inc.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * AjaxUpload
+ *
+ * Copyright 2013 by Thomas Jakobi <thomas.jakobi@partout.info>
+ *
+ * AjaxUpload is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * AjaxUpload is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * AjaxUpload; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package ajaxupload
+ * @subpackage classfile
+ *
+ * Properties Russian Lexicon Entries for AjaxUpload
+ */
+$_lang['prop_ajaxupload.uid'] = 'Уникальный id очереди загрузки';
+$_lang['prop_ajaxupload.language'] = 'Язык сниппета/Javascript-а';
+$_lang['prop_ajaxupload.allowedExtensions'] = 'Разрешённые расширения для загрузки';
+$_lang['prop_ajaxupload.maxFilesizeMb'] = 'Максимальный размер одного заргужаемого файла';
+$_lang['prop_ajaxupload.maxFiles'] = 'Максимальное число файлов для загрузки';
+$_lang['prop_ajaxupload.thumbX'] = 'Горизонтальный размер генерируемого эскиза';
+$_lang['prop_ajaxupload.thumbY'] = 'Вертикальный размер генерируемого эскиза';
+$_lang['prop_ajaxupload.addJquery'] = 'Добавить вызов jQuery в конец body';
+$_lang['prop_ajaxupload.addJscript'] = 'Добавить javascript сниппета и вызов скрипта fileuploader script в конец body';
+$_lang['prop_ajaxupload.addCss'] = 'Добавить css сниппета в конец head';
+
+$_lang['prop_ajaxupload2formit.ajaxuploadUid'] = 'Уникальный id очереди загрузки';
+$_lang['prop_ajaxupload2formit.ajaxuploadFieldname'] = 'Поле Formit для сохранения (уже) загруженных имён/путей файлов';
+$_lang['prop_ajaxupload2formit.ajaxuploadTarget'] = 'Путь для сохранения (уже) загруженных файлов (относительно $modx->getOption[\'assetsPath\'])';
+$_lang['prop_ajaxupload2formit.ajaxuploadFieldformat'] = 'Формат данных, сохранённых в \'ajaxuploadFieldname\'';
+
+$_lang['prop_formit2ajaxupload.ajaxuploadUid'] = $_lang['prop_ajaxupload2formit.ajaxuploadUid'];
+$_lang['prop_formit2ajaxupload.ajaxuploadFieldname'] = $_lang['prop_ajaxupload2formit.ajaxuploadFieldname'];
+$_lang['prop_formit2ajaxupload.ajaxuploadTarget'] = $_lang['prop_ajaxupload2formit.ajaxuploadTarget'];
+$_lang['prop_formit2ajaxupload.ajaxuploadFieldformat'] = $_lang['prop_ajaxupload2formit.ajaxuploadFieldformat'];
