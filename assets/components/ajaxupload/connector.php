c130845f669f9674ac949ae4432e3e502892b23f
Thomas Jakobi <thomas.jakobi@partout.info>
2015-10-26 09:08:01 +0100

    Refactoring code

--- assets/components/ajaxupload/connector.php
+++ assets/components/ajaxupload/connector.php
@@ -12,41 +12,24 @@
 /* Allow anonymous users */
 define('MODX_REQP', false);
 
-include_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
-if (!defined('MODX_CORE_PATH')) {
-    include_once dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/config.core.php';
-}
-if (!defined('MODX_CORE_PATH')) {
-    exit('config.core.php not found');
-}
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 
-$ajaxuploadCorePath = realpath($modx->getOption('ajaxupload.core_path', null, $modx->getOption('core_path') . 'components/ajaxupload/')) . '/';
+$corePath = $modx->getOption('ajaxupload.core_path', null, $modx->getOption('core_path') . 'components/ajaxupload/');
 
-if (!$modx->loadClass('AjaxUpload', $modx->getOption('ajaxupload.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/ajaxupload/') . 'model/ajaxupload/', true, true)) {
-    $modx->log(modX::LOG_LEVEL_ERROR, 'Could not load AjaxUpload class.', '', 'AjaxUpload');
-    return '';
-}
+/** @var AjaxUpload $ajaxupload */
+$ajaxupload = $modx->getService('ajaxupload', 'AjaxUpload', $corePath . 'model/ajaxupload/', array(
+    'core_path' => $corePath
+));
 
 if ($_REQUEST['action'] == 'web/upload') {
-    $version = $modx->getVersionData();
-    if (version_compare($version['full_version'], '2.1.1-pl') >= 0) {
-        if ($modx->user->hasSessionContext($modx->context->get('key'))) {
-            $_SERVER['HTTP_MODAUTH'] = $_SESSION["modx.{$modx->context->get('key')}.user.token"];
-        } else {
-            $_SESSION["modx.{$modx->context->get('key')}.user.token"] = 0;
-            $_SERVER['HTTP_MODAUTH'] = 0;
-        }
-    } else {
-        $_SERVER['HTTP_MODAUTH'] = $modx->site_id;
-    }
-    $_REQUEST['HTTP_MODAUTH'] = $_SERVER['HTTP_MODAUTH'];
+    $_SERVER['HTTP_MODAUTH'] = $modx->user->getUserToken($modx->context->get('key'));
 }
 
-/* handle request */
-$path = $modx->getOption('processorsPath', null, $ajaxuploadCorePath . 'processors/');
+// Handle request
+$path = $modx->getOption('processorsPath', $ajaxupload->config, $corePath . 'processors/');
 $modx->request->handleRequest(array(
     'processors_path' => $path,
-    'location' => '',
+    'location' => ''
 ));
