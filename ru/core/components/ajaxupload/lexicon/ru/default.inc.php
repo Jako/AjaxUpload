ef4979c9597e169649f208770da77a0cd3e6e807
Thomas Jakobi <thomas.jakobi@partout.info>
2016-12-19 11:13:58 +0100

    New translations

---
+++ ru/core/components/ajaxupload/lexicon/ru/default.inc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Default Lexicon Entries for AjaxUpload
+ *
+ * @package ajaxupload
+ * @subpackage lexicon
+ */
+$_lang['ajaxupload'] = 'AjaxUpload';
+$_lang['ajaxupload.notWritable'] = "Server error. Upload directory isn't writable.";
+$_lang['ajaxupload.targetNotWritable'] = "Could not copy the uploaded file to the target folder. Does the folder exist and is it writable?";
+$_lang['ajaxupload.noFile'] = "No files were uploaded.";
+$_lang['ajaxupload.emptyFile'] = "File is empty.";
+$_lang['ajaxupload.largeFile'] = "File is too large.";
+$_lang['ajaxupload.wrongExtension'] = "File has an invalid extension, it should be one of [[+allowedExtensions]].";
+$_lang['ajaxupload.saveError'] = "Could not save uploaded file. The upload was cancelled or a server error encountered.";
+$_lang['ajaxupload.maxFiles'] = "Only [[+maxFiles]] different files are allowed to upload.";
+$_lang['ajaxupload.notFound'] = "File is not found.";
+$_lang['ajaxupload.thumbnailGenerationProblem'] = "Thumbnail generation failed.";
+$_lang['ajaxupload.dropArea'] = "Drop files here to upload.";
+$_lang['ajaxupload.uploadButton'] = "Upload";
+$_lang['ajaxupload.deleteButton'] = "Delete";
+$_lang['ajaxupload.clearButton'] = "Delete all";
+$_lang['ajaxupload.cancel'] = "Cancel";
+$_lang['ajaxupload.failed'] = "Failed";
+$_lang['ajaxupload.typeError'] = "Unfortunately the file(s) you selected weren't the type we were expecting. Only {extensions} files are allowed.";
+$_lang['ajaxupload.sizeError'] = "{file} is too large, maximum file size is {sizeLimit}.";
+$_lang['ajaxupload.minSizeError'] = "{file} is too small, minimum file size is {minSizeLimit}.";
+$_lang['ajaxupload.emptyError'] = "{file} is empty, please select files again without it.";
+$_lang['ajaxupload.onLeave'] = "The files are being uploaded, if you leave now the upload will be cancelled.";
