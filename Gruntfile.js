5fc651a87beb5aa4ed645232ec236e864d4f4a2e
Thomas Jakobi <thomas.jakobi@partout.info>
2018-09-01 18:58:06 +0200

    Grunt with node-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,7 @@ module.exports = function (grunt) {
         },
         sass: {
             options: {
+                implementation: require('node-sass'),
                 outputStyle: 'expanded',
                 sourcemap: false
             },
