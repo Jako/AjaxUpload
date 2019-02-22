dba1f4866f9c23402b7b66e43d9648162ce106cd
Thomas Jakobi <thomas.jakobi@partout.info>
2019-02-22 21:20:25 +0100

    New translations setting.inc.php (German)

---
+++ core/components/ajaxupload/lexicon/de/setting.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Setting Lexicon Entries for AjaxUpload
+ *
+ * @package ajaxupload
+ * @subpackage lexicon
+ */
+$_lang['setting_ajaxupload.cache_expires'] = 'Expire Time';
+$_lang['setting_ajaxupload.cache_expires_desc'] = 'Expire Time of the AjaxUpload cache (in hours)';
+$_lang['setting_ajaxupload.debug'] = 'Debug';
+$_lang['setting_ajaxupload.debug_desc'] = 'Log debug informations in MODX error log.';
