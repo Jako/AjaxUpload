14251b769fd63ed0050e23f15547407d36731584
Thomas Jakobi <thomas.jakobi@partout.info>
2019-02-22 21:22:52 +0100

    New translations setting.inc.php (German)

--- core/components/ajaxupload/lexicon/de/setting.inc.php
+++ core/components/ajaxupload/lexicon/de/setting.inc.php
@@ -5,7 +5,7 @@
  * @package ajaxupload
  * @subpackage lexicon
  */
-$_lang['setting_ajaxupload.cache_expires'] = 'Expire Time';
-$_lang['setting_ajaxupload.cache_expires_desc'] = 'Expire Time of the AjaxUpload cache (in hours)';
+$_lang['setting_ajaxupload.cache_expires'] = 'Ablaufzeit';
+$_lang['setting_ajaxupload.cache_expires_desc'] = 'Ablaufzeit des AjaxUpload-Cache (in Stunden)';
 $_lang['setting_ajaxupload.debug'] = 'Debug';
-$_lang['setting_ajaxupload.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_ajaxupload.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
