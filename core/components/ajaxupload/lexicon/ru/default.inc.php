da2fa86ca3564eb2702ab18d67cb3f777cddb220
Thomas Jakobi <thomas.jakobi@partout.info>
2019-05-16 09:44:28 +0200

    New translations default.inc.php (Russian)

--- core/components/ajaxupload/lexicon/ru/default.inc.php
+++ core/components/ajaxupload/lexicon/ru/default.inc.php
@@ -5,24 +5,25 @@
  * @package ajaxupload
  * @subpackage lexicon
  */
-$_lang['ajaxupload.notWritable'] = "Ошибка сервера. Каталог загрузки закрыт для записи.";
-$_lang['ajaxupload.targetNotWritable'] = "Не удалось скопировать загруженный файл в целевую папку. Папка существует и доступен для записи?";
-$_lang['ajaxupload.noFile'] = "Файлы не загружены.";
-$_lang['ajaxupload.emptyFile'] = "Файл пуст.";
-$_lang['ajaxupload.largeFile'] = "Файл слишком велик.";
-$_lang['ajaxupload.wrongExtension'] = "У файла неверное расширение, он может быть только [[+allowedExtensions]].";
-$_lang['ajaxupload.saveError'] = "Загруженный файл не сохранён. Загрузка отменена или произошла ошибка сервера.";
-$_lang['ajaxupload.maxFiles'] = "Только [[+maxFiles]] файлов можно загрузить.";
-$_lang['ajaxupload.notFound'] = "Файл не найден.";
-$_lang['ajaxupload.thumbnailGenerationProblem'] = "Эскиз не создан.";
-$_lang['ajaxupload.dropArea'] = "Перетащите файлы сюда.";
-$_lang['ajaxupload.uploadButton'] = "Прикрепить файл";
-$_lang['ajaxupload.deleteButton'] = "Удалить";
-$_lang['ajaxupload.clearButton'] = "Удалить все";
-$_lang['ajaxupload.cancel'] = "Отмена";
-$_lang['ajaxupload.failed'] = "Не вышло";
-$_lang['ajaxupload.typeError'] = "К сожалению файл(ы) этого типа загрузить нельзя. Только файлы {extensions}.";
-$_lang['ajaxupload.sizeError'] = "{file} слишком велик, максимальный размер — {sizeLimit}.";
-$_lang['ajaxupload.minSizeError'] = "{file} слишком мал, минимальный размер — {minSizeLimit}.";
-$_lang['ajaxupload.emptyError'] = "{file} пуст. Пожалуйста, выберите файлы без него.";
-$_lang['ajaxupload.onLeave'] = "Файлы загружаются, если Вы закроете страницу, загрузка будет отменена.";
+$_lang['ajaxupload.notWritable'] = 'Ошибка сервера. Каталог загрузки закрыт для записи.';
+$_lang['ajaxupload.targetNotWritable'] = 'Не удалось скопировать загруженный файл в целевую папку. Папка существует и доступен для записи?';
+$_lang['ajaxupload.targetNotCreatable'] = '';
+$_lang['ajaxupload.noFile'] = 'Файлы не загружены.';
+$_lang['ajaxupload.emptyFile'] = 'Файл пуст.';
+$_lang['ajaxupload.largeFile'] = 'Файл слишком велик.';
+$_lang['ajaxupload.wrongExtension'] = 'У файла неверное расширение, он может быть только [[+allowedExtensions]].';
+$_lang['ajaxupload.saveError'] = 'Загруженный файл не сохранён. Загрузка отменена или произошла ошибка сервера.';
+$_lang['ajaxupload.maxFiles'] = 'Только [[+maxFiles]] файлов можно загрузить.';
+$_lang['ajaxupload.notFound'] = 'Файл не найден.';
+$_lang['ajaxupload.thumbnailGenerationProblem'] = 'Эскиз не создан.';
+$_lang['ajaxupload.dropArea'] = 'Перетащите файлы сюда.';
+$_lang['ajaxupload.uploadButton'] = 'Прикрепить файл';
+$_lang['ajaxupload.deleteButton'] = 'Удалить';
+$_lang['ajaxupload.clearButton'] = 'Удалить все';
+$_lang['ajaxupload.cancel'] = 'Отмена';
+$_lang['ajaxupload.failed'] = 'Не вышло';
+$_lang['ajaxupload.typeError'] = 'К сожалению файл(ы) этого типа загрузить нельзя. Только файлы {extensions}.';
+$_lang['ajaxupload.sizeError'] = '{file} слишком велик, максимальный размер — {sizeLimit}.';
+$_lang['ajaxupload.minSizeError'] = '{file} слишком мал, минимальный размер — {minSizeLimit}.';
+$_lang['ajaxupload.emptyError'] = '{file} пуст. Пожалуйста, выберите файлы без него.';
+$_lang['ajaxupload.onLeave'] = 'Файлы загружаются, если Вы закроете страницу, загрузка будет отменена.';
