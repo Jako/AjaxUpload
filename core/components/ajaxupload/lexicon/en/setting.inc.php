fbac5e19b8867a9c1c7d89e14ce9502ca2629f9f
Thomas Jakobi <thomas.jakobi@partout.info>
2021-11-16 15:08:48 +0100

    Update lexicon

--- core/components/ajaxupload/lexicon/en/setting.inc.php
+++ core/components/ajaxupload/lexicon/en/setting.inc.php
@@ -8,4 +8,4 @@
 $_lang['setting_ajaxupload.cache_expires'] = 'Expire Time';
 $_lang['setting_ajaxupload.cache_expires_desc'] = 'Expire Time of the AjaxUpload cache (in hours)';
 $_lang['setting_ajaxupload.debug'] = 'Debug';
-$_lang['setting_ajaxupload.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_ajaxupload.debug_desc'] = 'Log debug information in the MODX error log.';
