ff32c61bea1ebe78e04e74ebcfbbc92f3954a76a
Thomas Jakobi <thomas.jakobi@partout.info>
2022-03-22 14:36:50 +0100

    Update lexicon

--- core/components/ajaxupload/lexicon/en/default.inc.php
+++ core/components/ajaxupload/lexicon/en/default.inc.php
@@ -5,7 +5,7 @@
  * @package ajaxupload
  * @subpackage lexicon
  */
-$_lang['ajaxupload.notWritable'] = 'Server error. Upload directory isn\'t writable.';
+$_lang['ajaxupload.notWritable'] = 'Server error. Upload directory isn’t writable.';
 $_lang['ajaxupload.targetNotWritable'] = 'Could not copy the uploaded file to the target folder. Does the folder exist and is it writable?';
 $_lang['ajaxupload.targetNotCreatable'] = 'Could not create the target folder!';
 $_lang['ajaxupload.noFile'] = 'No files were uploaded.';
@@ -22,7 +22,7 @@ $_lang['ajaxupload.deleteButton'] = 'Delete';
 $_lang['ajaxupload.clearButton'] = 'Delete all';
 $_lang['ajaxupload.cancel'] = 'Cancel';
 $_lang['ajaxupload.failed'] = 'Failed';
-$_lang['ajaxupload.typeError'] = 'Unfortunately the file(s) you selected weren\'t the type we were expecting. Only {extensions} files are allowed.';
+$_lang['ajaxupload.typeError'] = 'Unfortunately the file(s) you selected weren’t the type we were expecting. Only {extensions} files are allowed.';
 $_lang['ajaxupload.sizeError'] = '{file} is too large, maximum file size is {sizeLimit}.';
 $_lang['ajaxupload.minSizeError'] = '{file} is too small, minimum file size is {minSizeLimit}.';
 $_lang['ajaxupload.emptyError'] = '{file} is empty, please select files again without it.';
