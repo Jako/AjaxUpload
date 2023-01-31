312c7251b9db51488578aa93b0f9e63f1c35ccad
Thomas Jakobi <thomas.jakobi@partout.info>
2023-02-01 00:18:28 +0100

    Update lexicon for 1.6.5

--- core/components/ajaxupload/lexicon/en/setting.inc.php
+++ core/components/ajaxupload/lexicon/en/setting.inc.php
@@ -9,3 +9,5 @@ $_lang['setting_ajaxupload.cache_expires'] = 'Expire Time';
 $_lang['setting_ajaxupload.cache_expires_desc'] = 'Expire Time of the AjaxUpload cache (in hours)';
 $_lang['setting_ajaxupload.debug'] = 'Debug';
 $_lang['setting_ajaxupload.debug_desc'] = 'Log debug information in the MODX error log.';
+$_lang['setting_ajaxupload.image_tpl'] = 'Image Template';
+$_lang['setting_ajaxupload.image_tpl_desc'] = 'Name of a chunk that contains the HTML code for displaying the uploaded image.';
