50ff709cc2fee1220160672122f574c39e43f4a0
Thomas Jakobi <thomas.jakobi@partout.info>
2023-09-04 13:36:31 +0200

    Normalize getAssociativeJson

--- core/components/ajaxupload/src/Snippets/Snippet.php
+++ core/components/ajaxupload/src/Snippets/Snippet.php
@@ -118,7 +118,7 @@ abstract class Snippet
      */
     protected function getAssociativeJson($value)
     {
-        return is_string($value) ? json_decode($value, true) : $value;
+        return (is_string($value)) ? json_decode($value, true) : $value;
     }
 
     /**
