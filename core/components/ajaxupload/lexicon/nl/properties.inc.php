aef44c9df4a51b9036acbe164512004f68c6c32c
Hugo Peek <hugo@fractal-farming.com>
2024-04-03 15:04:53 +0200

    Added translation using Weblate (Dutch)

---
+++ core/components/ajaxupload/lexicon/nl/properties.inc.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Properties Lexicon Entries for AjaxUpload
+ *
+ * @package ajaxupload
+ * @subpackage lexicon
+ */
+$_lang['ajaxupload.ajaxupload.addCss'] = '';
+$_lang['ajaxupload.ajaxupload.addJquery'] = '';
+$_lang['ajaxupload.ajaxupload.addJscript'] = '';
+$_lang['ajaxupload.ajaxupload.allowedExtensions'] = '';
+$_lang['ajaxupload.ajaxupload.language'] = '';
+$_lang['ajaxupload.ajaxupload.maxFiles'] = '';
+$_lang['ajaxupload.ajaxupload.maxFilesizeMb'] = '';
+$_lang['ajaxupload.ajaxupload.scriptTpl'] = '';
+$_lang['ajaxupload.ajaxupload.thumbX'] = '';
+$_lang['ajaxupload.ajaxupload.thumbY'] = '';
+$_lang['ajaxupload.ajaxupload.uid'] = '';
+$_lang['ajaxupload.ajaxupload.uploadSectionTpl'] = '';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadAllowOverwrite'] = '';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadClearQueue'] = '';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadFieldformat'] = '';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadFieldname'] = '';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadTarget'] = '';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadUid'] = '';
+$_lang['ajaxupload.ajaxupload2formit.csv'] = '';
+$_lang['ajaxupload.ajaxupload2formit.json'] = '';
+$_lang['ajaxupload.ajaxuploadrequired.ajaxuploadRequiredMessage'] = '';
+$_lang['ajaxupload.formit2ajaxupload.ajaxuploadFieldformat'] = '';
+$_lang['ajaxupload.formit2ajaxupload.ajaxuploadFieldname'] = '';
+$_lang['ajaxupload.formit2ajaxupload.ajaxuploadTarget'] = '';
+$_lang['ajaxupload.formit2ajaxupload.ajaxuploadUid'] = '';
+$_lang['ajaxupload.formit2ajaxupload.csv'] = '';
+$_lang['ajaxupload.formit2ajaxupload.json'] = '';
