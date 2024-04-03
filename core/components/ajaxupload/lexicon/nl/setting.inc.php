cd357a8d092105b74e7546dda2ea7de2eb0bc8d9
Hugo Peek <hugo@fractal-farming.com>
2024-04-03 15:42:17 +0200

    Added translation using Weblate (Dutch)

---
+++ core/components/ajaxupload/lexicon/nl/setting.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Setting Lexicon Entries for AjaxUpload
+ *
+ * @package ajaxupload
+ * @subpackage lexicon
+ */
+$_lang['setting_ajaxupload.cache_expires'] = '';
+$_lang['setting_ajaxupload.cache_expires_desc'] = '';
+$_lang['setting_ajaxupload.debug'] = '';
+$_lang['setting_ajaxupload.debug_desc'] = '';
+$_lang['setting_ajaxupload.image_tpl'] = '';
+$_lang['setting_ajaxupload.image_tpl_desc'] = '';
