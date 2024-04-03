637b2e0239f887f2619901a6728ab33e8d44d4f6
Hugo Peek <hugo@fractal-farming.com>
2024-04-03 15:46:05 +0200

    Added translation using Weblate (Dutch)

---
+++ core/components/ajaxupload/lexicon/nl/default.inc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Default Lexicon Entries for AjaxUpload
+ *
+ * @package ajaxupload
+ * @subpackage lexicon
+ */
+$_lang['ajaxupload.cancel'] = '';
+$_lang['ajaxupload.clearButton'] = '';
+$_lang['ajaxupload.deleteButton'] = '';
+$_lang['ajaxupload.dropArea'] = '';
+$_lang['ajaxupload.emptyError'] = '';
+$_lang['ajaxupload.emptyFile'] = '';
+$_lang['ajaxupload.failed'] = '';
+$_lang['ajaxupload.largeFile'] = '';
+$_lang['ajaxupload.maxFiles'] = '';
+$_lang['ajaxupload.minSizeError'] = '';
+$_lang['ajaxupload.noFile'] = '';
+$_lang['ajaxupload.notFound'] = '';
+$_lang['ajaxupload.notWritable'] = '';
+$_lang['ajaxupload.onLeave'] = '';
+$_lang['ajaxupload.saveError'] = '';
+$_lang['ajaxupload.sizeError'] = '';
+$_lang['ajaxupload.targetNotCreatable'] = '';
+$_lang['ajaxupload.targetNotWritable'] = '';
+$_lang['ajaxupload.thumbnailGenerationProblem'] = '';
+$_lang['ajaxupload.typeError'] = '';
+$_lang['ajaxupload.uploadButton'] = '';
+$_lang['ajaxupload.uploadRequired'] = '';
+$_lang['ajaxupload.wrongExtension'] = '';
