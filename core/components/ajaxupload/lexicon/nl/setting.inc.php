65e90981e2dfcd61cbab75e11ff736a84812a8b9
Hugo Peek <hugo@fractal-farming.com>
2024-04-03 14:18:26 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MODX Extras/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/ajaxupload/system-settings/nl/

--- core/components/ajaxupload/lexicon/nl/setting.inc.php
+++ core/components/ajaxupload/lexicon/nl/setting.inc.php
@@ -5,9 +5,9 @@
  * @package ajaxupload
  * @subpackage lexicon
  */
-$_lang['setting_ajaxupload.cache_expires'] = '';
-$_lang['setting_ajaxupload.cache_expires_desc'] = '';
-$_lang['setting_ajaxupload.debug'] = '';
-$_lang['setting_ajaxupload.debug_desc'] = '';
-$_lang['setting_ajaxupload.image_tpl'] = '';
-$_lang['setting_ajaxupload.image_tpl_desc'] = '';
+$_lang['setting_ajaxupload.cache_expires'] = 'Vervaltijd';
+$_lang['setting_ajaxupload.cache_expires_desc'] = 'Vervaltijd van de AjaxUpload cache (in uren)';
+$_lang['setting_ajaxupload.debug'] = 'Debug';
+$_lang['setting_ajaxupload.debug_desc'] = 'Log debug-informatie in het MODX log bestand.';
+$_lang['setting_ajaxupload.image_tpl'] = 'Template voor afbeelding';
+$_lang['setting_ajaxupload.image_tpl_desc'] = 'Naam van een chunk die de HTML-code bevat om de geüploade afbeelding weer te geven.';
