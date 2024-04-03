36209d9695b262bb3ce36dfae5501c91859d6820
Hugo Peek <hugo@fractal-farming.com>
2024-04-03 13:41:32 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: MODX Extras/Properties
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/ajaxupload/properties/nl/

--- core/components/ajaxupload/lexicon/nl/properties.inc.php
+++ core/components/ajaxupload/lexicon/nl/properties.inc.php
@@ -5,7 +5,7 @@
  * @package ajaxupload
  * @subpackage lexicon
  */
-$_lang['ajaxupload.ajaxupload.addCss'] = 'Voeg de CSS snippet toe aan het eind van de head.';
+$_lang['ajaxupload.ajaxupload.addCss'] = 'Voeg de CSS toe aan het eind van de head.';
 $_lang['ajaxupload.ajaxupload.addJquery'] = 'Voeg jQuery script toe aan het eind van de body.';
 $_lang['ajaxupload.ajaxupload.addJscript'] = 'Voeg de Javascript snippet en het fileuploader script toe aan het eind van de body.';
 $_lang['ajaxupload.ajaxupload.allowedExtensions'] = 'Deze bestandsextensies zijn toegestaan.';
@@ -18,7 +18,7 @@ $_lang['ajaxupload.ajaxupload.thumbY'] = 'Hoogte van de gegenereerde thumbnail.'
 $_lang['ajaxupload.ajaxupload.uid'] = 'Uniek ID voor upload wachtrij.';
 $_lang['ajaxupload.ajaxupload.uploadSectionTpl'] = 'Naam van een chunk waar de HTML code voor het upload gedeelte in staat.';
 $_lang['ajaxupload.ajaxupload2formit.ajaxuploadAllowOverwrite'] = 'Bestaande bestanden met dezelfde naam mogen overschreven worden.';
-$_lang['ajaxupload.ajaxupload2formit.ajaxuploadClearQueue'] = 'Leeg de wachtrij als de hook succesvol heeft gedraaid.';
+$_lang['ajaxupload.ajaxupload2formit.ajaxuploadClearQueue'] = 'Leeg de wachtrij nadat de hook succesvol heeft gedraaid.';
 $_lang['ajaxupload.ajaxupload2formit.ajaxuploadFieldformat'] = 'Formaat van de data opgeslagen in `ajaxuploadFieldname`.';
 $_lang['ajaxupload.ajaxupload2formit.ajaxuploadFieldname'] = 'Het Formit veld waarin de paden/bestandsnamen van reeds voltooide uploads worden opgeslagen.';
 $_lang['ajaxupload.ajaxupload2formit.ajaxuploadTarget'] = 'Doel locatie voor reeds geuploade bestanden (relatief aan $modx->getOption[\'assetsPath\']). Zorg ervoor dat PHP schrijfrechten heeft op deze locatie.';
