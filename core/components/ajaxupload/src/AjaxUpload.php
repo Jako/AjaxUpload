fbc3a5c6dc597743c2d2fd45083d7cc54946093a
Thomas Jakobi <thomas.jakobi@partout.info>
2025-02-28 12:25:40 +0100

    Update to 2.0.0-b7

--- core/components/ajaxupload/src/AjaxUpload.php
+++ core/components/ajaxupload/src/AjaxUpload.php
@@ -42,7 +42,7 @@ class AjaxUpload
      * The version
      * @var string $version
      */
-    public $version = '2.0.0-b1';
+    public $version = '2.0.0-b7';
 
     /**
      * The class options
