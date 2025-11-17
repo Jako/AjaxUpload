42a6e47dc6d3aaf929025f06941196a76a951654
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:42:59 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/ajaxupload/lexicon/es/default.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Default Lexicon Entries for AjaxUpload
+ *
+ * @package ajaxupload
+ * @subpackage lexicon
+ */
+$_lang['ajaxupload'] = '';
+$_lang['ajaxupload.cacheNotCreatable'] = '';
+$_lang['ajaxupload.filledFileNotFound'] = '';
+$_lang['ajaxupload.missingParameterAjaxuploadTarget'] = '';
+$_lang['ajaxupload.targetNotCreatable'] = '';
+$_lang['ajaxupload.uploadRequired'] = '';
