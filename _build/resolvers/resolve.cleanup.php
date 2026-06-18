f0357dc25e04665554459fe800dd66d164ad8915
Thomas Jakobi <thomas.jakobi@partout.info>
2026-06-18 18:55:44 +0200

    Add GLightbox to Zensical

--- _build/resolvers/resolve.cleanup.php
+++ _build/resolvers/resolve.cleanup.php
@@ -11,89 +11,89 @@
 
 $success = false;
 
-if ($object->xpdo) {
-    if (!function_exists('recursiveRemoveFolder')) {
-        function recursiveRemoveFolder($dir)
-        {
-            $files = array_diff(scandir($dir), ['.', '..']);
-            foreach ($files as $file) {
-                (is_dir("$dir/$file")) ? recursiveRemoveFolder($dir . '/' . $file) : unlink($dir . '/' . $file);
-            }
-            return rmdir($dir);
+if (!function_exists('recursiveRemoveFolder')) {
+    function recursiveRemoveFolder($dir)
+    {
+        $files = array_diff(scandir($dir), ['.', '..']);
+        foreach ($files as $file) {
+            (is_dir("$dir/$file")) ? recursiveRemoveFolder($dir . '/' . $file) : unlink($dir . '/' . $file);
         }
+        return rmdir($dir);
     }
+}
 
-    if (!function_exists('cleanupFolders')) {
-        function cleanupFolders($modx, $corePath, $assetsPath, $cleanup, $package, $version)
-        {
-            $paths = [
-                'core' => $corePath,
-                'assets' => $assetsPath,
-            ];
-            $countFiles = 0;
-            $countFolders = 0;
-            foreach ($cleanup as $folder => $files) {
-                foreach ($files as $file) {
-                    $legacyFile = $paths[$folder] . $file;
-                    if (file_exists($legacyFile)) {
-                        if (is_dir($legacyFile)) {
-                            recursiveRemoveFolder($legacyFile);
-                            $countFolders++;
-                        } else {
-                            unlink($legacyFile);
-                            $countFiles++;
-                        }
+if (!function_exists('cleanupFolders')) {
+    function cleanupFolders($modx, $corePath, $assetsPath, $cleanup, $package, $version)
+    {
+        $paths = [
+            'core' => $corePath,
+            'assets' => $assetsPath,
+        ];
+        $countFiles = 0;
+        $countFolders = 0;
+        foreach ($cleanup as $folder => $files) {
+            foreach ($files as $file) {
+                $legacyFile = $paths[$folder] . $file;
+                if (file_exists($legacyFile)) {
+                    if (is_dir($legacyFile)) {
+                        recursiveRemoveFolder($legacyFile);
+                        $countFolders++;
+                    } else {
+                        unlink($legacyFile);
+                        $countFiles++;
                     }
                 }
             }
-            if ($countFolders || $countFiles) {
-                $modx->log(xPDO::LOG_LEVEL_INFO, 'Removed ' . $countFiles . ' legacy files and ' . $countFolders . ' legacy folders before ' . $package . ' ' . $version . '.');
-            }
+        }
+        if ($countFolders || $countFiles) {
+            $modx->log(xPDO::LOG_LEVEL_INFO, 'Removed ' . $countFiles . ' legacy files and ' . $countFolders . ' legacy folders before ' . $package . ' ' . $version . '.');
         }
     }
+}
 
-    if (!function_exists('cleanupMenu')) {
-        function cleanupMenu($modx, $namespace, $newAction)
-        {
-            /** @var modAction[] $actions */
-            $actions = $modx->getIterator('modAction', [
-                'namespace:=' => $namespace,
-                'controller' => 'index'
+if (!function_exists('cleanupMenu')) {
+    function cleanupMenu($modx, $namespace, $newAction)
+    {
+        /** @var modAction[] $actions */
+        $actions = $modx->getIterator('modAction', [
+            'namespace:=' => $namespace,
+            'controller' => 'index'
+        ]);
+        foreach ($actions as $action) {
+            /** @var modMenu $menu */
+            $menu = $modx->getObject('modMenu', $action->get('id'));
+            if ($menu) {
+                $menu->set('action', $newAction);
+                $menu->save();
+            }
+            $action->remove();
+        }
+    }
+}
+
+if (!function_exists('cleanupPluginEvents')) {
+    function cleanupPluginEvents($modx, $plugin, $events)
+    {
+        foreach ($events as $event) {
+            $c = $modx->newQuery('modPluginEvent');
+            $c->leftJoin('modPlugin', 'Plugin', [
+                'modPluginEvent.pluginid = Plugin.id'
             ]);
-            foreach ($actions as $action) {
-                /** @var modMenu $menu */
-                $menu = $modx->getObject('modMenu', $action->get('id'));
-                if ($menu) {
-                    $menu->set('action', $newAction);
-                    $menu->save();
-                }
-                $action->remove();
+            $c->where([
+                'event' => $event,
+                'Plugin.name' => $plugin
+            ]);
+            /** @var modPluginEvent $pluginEvent */
+            $pluginEvent = $modx->getObject('modPluginEvent', $c);
+            if ($pluginEvent) {
+                $pluginEvent->remove();
+                $modx->log(xPDO::LOG_LEVEL_INFO, 'Removed ' . $event . ' from ' . $plugin . ' plugin.');
             }
         }
     }
+}
 
-    if (!function_exists('cleanupPluginEvents')) {
-        function cleanupPluginEvents($modx, $plugin, $events)
-        {
-            foreach ($events as $event) {
-                $c = $modx->newQuery('modPluginEvent');
-                $c->leftJoin('modPlugin', 'Plugin', [
-                    'modPluginEvent.pluginid = Plugin.id'
-                ]);
-                $c->where([
-                    'event' => $event,
-                    'Plugin.name' => $plugin
-                ]);
-                /** @var modPluginEvent $pluginEvent */
-                $pluginEvent = $modx->getObject('modPluginEvent', $c);
-                if ($pluginEvent) {
-                    $pluginEvent->remove();
-                    $modx->log(xPDO::LOG_LEVEL_INFO, 'Removed ' . $event . ' from ' . $plugin . ' plugin.');
-                }
-            }
-        }
-    }
-
+if ($object->xpdo) {
     /** @var xPDO $modx */
     $modx =& $object->xpdo;
 
@@ -149,7 +149,7 @@ if ($object->xpdo) {
                         'components/ajaxupload/processors/web/upload.class.php',
                     ]
                 ];
-                cleanupFolders($modx, $corePath, $assetsPath, $cleanup, 'AjaxUpload', '1.6.0');
+                cleanupFolders($modx, $corePath, $assetsPath, $cleanup, 'AjaxUpload', '2.0.0');
             }
             $success = true;
             break;
